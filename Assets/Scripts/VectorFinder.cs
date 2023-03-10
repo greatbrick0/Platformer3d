8e3c2a640e666102fb832ea3e6a533258a88e24f
Spencer <speharley@gmail.com>
2023-03-10 16:57:27 -0500

    Added VectorFinder Script

---
+++ Assets/Scripts/VectorFinder.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorFinder : MonoBehaviour
+{
+    public enum Direction {
+        Upwards,
+        Downwards,
+        Left,
+        DoubleUp,
+        DoubleForward,
+        JumpForward
+    }
+
+    public Direction direction = Direction.DoubleForward;
+
+    public Vector3 FindVector()
+    {
+        switch (direction)
+        {
+            case Direction.Upwards:
+                return new Vector3(0, 1, 0);
+            case Direction.Downwards:
+                return new Vector3(0, -1, 0);
+            case Direction.Left:
+                return new Vector3(-1, 0, 0);
+            case Direction.DoubleUp:
+                return new Vector3(0, 2, 0);
+            case Direction.DoubleForward:
+                return new Vector3(0, 0, 2);
+            case Direction.JumpForward:
+                return new Vector3(0, 1, 1);
+            default:
+                return new Vector3(0, 0, 0);
+        }
+    }
+}
